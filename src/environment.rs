95dc431d0c9fef18bac1bbcb774b12c53c3a4b41
Markus Klein <markus-klein@live.de>
2016-12-13 03:56:50 +0100

    first prototype for listing drivers

--- src/environment.rs
+++ src/environment.rs
@@ -9,6 +9,15 @@ pub struct Environment {
     handle: raw::SQLHENV,
 }
 
+/// Struct holding information available on a driver.
+///
+/// Can be obtained via `Environment::drivers`
+#[derive(Debug)]
+pub struct DriverInfo {
+    description: String,
+    attributes: String,
+}
+
 impl Environment {
     /// Allocates a new ODBC Environment
     ///
@@ -32,6 +41,85 @@ impl Environment {
 
             Ok(result)
         }
+    }
+
+    /// Stores all driver description and attributes in a Vec
+    pub fn drivers(&self) -> Result<Vec<DriverInfo>> {
+        // Iterate twice, once for reading the maximum required buffer lengths so we can read
+        // everything without truncating and a second time for actually storing the values
+        let string_buf = std::ptr::null_mut();
+        let mut desc_length_out: raw::SQLSMALLINT = 0;
+        let mut attr_length_out: raw::SQLSMALLINT = 0;
+        let mut max_desc = 0;
+        let mut max_attr = 0;
+        let mut count = 0;
+        let mut result;
+        unsafe {
+            // although the rather lengthy function call kind of blows the call, let's do the first
+            // one using SQL_FETCH_FIRST, so we list all drivers independent from environment state
+            result = raw::SQLDrivers(self.handle,
+                                     raw::SQL_FETCH_FIRST,
+                                     string_buf,
+                                     0,
+                                     &mut desc_length_out as *mut raw::SQLSMALLINT,
+                                     string_buf,
+                                     0,
+                                     &mut attr_length_out as *mut raw::SQLSMALLINT);
+        }
+        loop {
+            match result {
+                raw::SQL_SUCCESS |
+                raw::SQL_SUCCESS_WITH_INFO => {
+                    count += 1;
+                    max_desc = std::cmp::max(max_desc, desc_length_out);
+                    max_attr = std::cmp::max(max_attr, attr_length_out);
+                }
+                raw::SQL_NO_DATA => break,
+                raw::SQL_ERROR => return Err(Error {}),
+                _ => unreachable!(),
+            }
+            unsafe {
+                result = raw::SQLDrivers(self.handle,
+                                         raw::SQL_FETCH_NEXT,
+                                         string_buf,
+                                         0,
+                                         &mut desc_length_out as *mut raw::SQLSMALLINT,
+                                         string_buf,
+                                         0,
+                                         &mut attr_length_out as *mut raw::SQLSMALLINT);
+            }
+        }
+
+        let mut driver_list = Vec::with_capacity(count);
+        loop {
+            let mut description_buffer: Vec<_> = (0..(max_desc + 1)).map(|_| 0u8).collect();
+            let mut attribute_buffer: Vec<_> = (0..(max_attr + 1)).map(|_| 0u8).collect();
+            unsafe {
+                result = raw::SQLDrivers(self.handle,
+                                         // Its ok to use fetch next here, since we know
+                                         // last state has been SQL_NO_DATA
+                                         raw::SQL_FETCH_NEXT,
+                                         &mut description_buffer[0] as *mut u8,
+                                         max_desc + 1,
+                                         std::ptr::null_mut(),
+                                         &mut attribute_buffer[0] as *mut u8,
+                                         max_attr + 1,
+                                         std::ptr::null_mut());
+            }
+            match result {
+                raw::SQL_SUCCESS |
+                raw::SQL_SUCCESS_WITH_INFO => {
+                    driver_list.push(DriverInfo {
+                        description: String::from_utf8(description_buffer).unwrap(),
+                        attributes: String::from_utf8(attribute_buffer).unwrap(),
+                    })
+                }
+                raw::SQL_ERROR => return Err(Error {}),
+                raw::SQL_NO_DATA => break,
+                _ => unreachable!(),
+            }
+        }
+        Ok(driver_list)
     }
 
     /// Allows access to the raw ODBC handle
