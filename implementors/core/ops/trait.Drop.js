e10699a229b727f283b06fc0722707ff580d4b82
Konstantin Salikhov <kostya.salikhov@castle.co>
2017-03-16 11:41:43 +0400

    0.3.0 doc

--- implementors/core/ops/trait.Drop.js
+++ implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["odbc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='odbc/struct.Environment.html' title='odbc::Environment'>Environment</a>","impl&lt;'a&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='odbc/struct.DataSource.html' title='odbc::DataSource'>DataSource</a>&lt;'a&gt;",];
+implementors["odbc"] = ["impl&lt;'a, S:&nbsp;DataSourceState&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='odbc/struct.DataSource.html' title='odbc::DataSource'>DataSource</a>&lt;'a, S&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
-implementors["odbc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='odbc/enum.Error.html' title='odbc::Error'>Error</a>",];
+implementors["log"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='log/struct.SetLoggerError.html' title='log::SetLoggerError'>SetLoggerError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='log/struct.ShutdownLoggerError.html' title='log::ShutdownLoggerError'>ShutdownLoggerError</a>",];
+implementors["odbc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='odbc/struct.DiagnosticRecord.html' title='odbc::DiagnosticRecord'>DiagnosticRecord</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='odbc/struct.EnvAllocError.html' title='odbc::EnvAllocError'>EnvAllocError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
