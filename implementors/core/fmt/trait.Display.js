7317c18ebf2da9be6f5c4af3e57a1f37d4f3ab98
Konstantin Salikhov <kostya.salikhov@castle.co>
2017-04-28 15:50:46 +0400

    Update docs gh-pages

---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["log"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='log/enum.LogLevel.html' title='log::LogLevel'>LogLevel</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='log/enum.LogLevelFilter.html' title='log::LogLevelFilter'>LogLevelFilter</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='log/struct.SetLoggerError.html' title='log::SetLoggerError'>SetLoggerError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='log/struct.ShutdownLoggerError.html' title='log::ShutdownLoggerError'>ShutdownLoggerError</a>",];
+implementors["odbc"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='odbc/struct.DiagnosticRecord.html' title='odbc::DiagnosticRecord'>DiagnosticRecord</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='struct' href='odbc/struct.EnvAllocError.html' title='odbc::EnvAllocError'>EnvAllocError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- src/ffi.rs
+++
@@ -1,3 +0,0 @@
-//! Reexport odbc-sys as ffi
-extern crate odbc_sys;
-pub use self::odbc_sys::*;
