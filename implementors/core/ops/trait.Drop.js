1860a0914d206b2f73ca5099912fd2f821bc7b41
Konstantin Salikhov <kostya.salikhov@castle.co>
2017-05-02 10:01:31 +0400

    Update docs gh-pages

--- implementors/core/ops/trait.Drop.js
+++ implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["odbc"] = ["impl&lt;'a, S:&nbsp;DataSourceState&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='odbc/struct.DataSource.html' title='odbc::DataSource'>DataSource</a>&lt;'a, S&gt;",];
+implementors["odbc"] = ["impl&lt;'a, S:&nbsp;DataSourceState&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html\" title=\"trait core::ops::Drop\">Drop</a> for <a class=\"struct\" href=\"odbc/struct.DataSource.html\" title=\"struct odbc::DataSource\">DataSource</a>&lt;'a, S&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/str/trait.FromStr.js
+++ implementors/core/str/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["log"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='enum' href='log/enum.LogLevel.html' title='log::LogLevel'>LogLevel</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html' title='core::str::FromStr'>FromStr</a> for <a class='enum' href='log/enum.LogLevelFilter.html' title='log::LogLevelFilter'>LogLevelFilter</a>",];
+implementors["log"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"log/enum.LogLevel.html\" title=\"enum log::LogLevel\">LogLevel</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"log/enum.LogLevelFilter.html\" title=\"enum log::LogLevelFilter\">LogLevelFilter</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
