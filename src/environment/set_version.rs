32377b2b03e8c53c87b87179c86f95d1a0c2094f
Konstantin Salikhov <kostya.salikhov@castle.co>
2017-08-09 15:05:31 +0400

    migrate to odbc-sys 0.5.0

--- src/environment/set_version.rs
+++ src/environment/set_version.rs
@@ -1,5 +1,5 @@
 use super::{Raii, ffi, Return, Handle};
-use std;
+use std::os::raw::{c_void, c_ulong};
 
 impl Raii<ffi::Env> {
     pub fn set_odbc_version_3(&mut self) -> Return<()> {
@@ -7,7 +7,7 @@ impl Raii<ffi::Env> {
         match unsafe {
             ffi::SQLSetEnvAttr(self.handle(),
                                ffi::SQL_ATTR_ODBC_VERSION,
-                               ffi::SQL_OV_ODBC3 as *mut std::os::raw::c_void,
+                               ffi::SQL_OV_ODBC3 as c_ulong as *mut c_void,
                                0)
         } {
             ffi::SQL_SUCCESS => Return::Success(()),
