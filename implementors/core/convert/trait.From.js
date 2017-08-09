7656de59537c5b9fb1bff3e4f68ddbdbeeb74a91
Konstantin Salikhov <kostya.salikhov@castle.co>
2017-08-09 15:07:39 +0400

    Update docs gh-pages

---
+++ implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["odbc_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"odbc_sys/enum.OdbcVersion.html\" title=\"enum odbc_sys::OdbcVersion\">OdbcVersion</a>&gt; for <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.pointer.html\">*mut </a><a class=\"enum\" href=\"https://doc.rust-lang.org/nightly/std/os/raw/enum.c_void.html\" title=\"enum std::os::raw::c_void\">c_void</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["log"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"log/enum.LogLevel.html\" title=\"enum log::LogLevel\">LogLevel</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"log/enum.LogLevelFilter.html\" title=\"enum log::LogLevelFilter\">LogLevelFilter</a>","impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"log/struct.LogMetadata.html\" title=\"struct log::LogMetadata\">LogMetadata</a>&lt;'a&gt;","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"log/struct.LogLocation.html\" title=\"struct log::LogLocation\">LogLocation</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
