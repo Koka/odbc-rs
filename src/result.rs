3d3d0af3390542ab130ed6a1c31941b14cb61b5c
Markus Klein <markus-klein@live.de>
2017-08-17 00:47:18 +0200

    introduce safe::Environment

--- src/result.rs
+++ src/result.rs
@@ -1,5 +1,6 @@
 //! Result types to enabling handling of ODBC Errors
 use super::{DiagnosticRecord, GetDiagRec};
+use safe;
 use std::fmt::{Display, Formatter};
 use std;
 
@@ -37,6 +38,69 @@ impl<T> Return<T> {
                 }
                 Err(diag)
             }
+        }
+    }
+}
+
+
+// temporary glue code to odbc-safe
+pub fn try_into_option<T, E, D>(ret: safe::ReturnOption<T, E>, handle: &D) -> Result<Option<T>>
+where
+    D: GetDiagRec,
+{
+    match ret {
+        safe::ReturnOption::Success(value) => Ok(Some(value)),
+        safe::ReturnOption::Info(value) => {
+            let mut i = 1;
+            while let Some(diag) = handle.get_diag_rec(i) {
+                warn!("{}", diag);
+                i += 1;
+            }
+            Ok(Some(value))
+        }
+        safe::ReturnOption::NoData(_) => Ok(None),
+        safe::ReturnOption::Error(_) => {
+            // Return the first record
+            let diag = handle.get_diag_rec(1).unwrap();
+            error!("{}", diag);
+            let mut i = 2;
+            // log the rest
+            while let Some(diag) = handle.get_diag_rec(i) {
+                error!("{}", diag);
+                i += 1;
+            }
+            Err(diag)
+        }
+    }
+}
+
+// temporary glue code to odbc-safe
+pub fn into_result<T, E>(ret: safe::Return<T, E>) -> Result<T>
+where
+    T: GetDiagRec,
+    E: GetDiagRec,
+{
+    match ret {
+        safe::Return::Success(value) => Ok(value),
+        safe::Return::Info(value) => {
+            let mut i = 1;
+            while let Some(diag) = value.get_diag_rec(i) {
+                warn!("{}", diag);
+                i += 1;
+            }
+            Ok(value)
+        }
+        safe::Return::Error(value) => {
+            // Return the first record
+            let diag = value.get_diag_rec(1).unwrap();
+            error!("{}", diag);
+            let mut i = 2;
+            // log the rest
+            while let Some(diag) = value.get_diag_rec(i) {
+                error!("{}", diag);
+                i += 1;
+            }
+            Err(diag)
         }
     }
 }
---
+++ src/safe.rs
@@ -0,0 +1,2 @@
+extern crate odbc_safe;
+pub use self::odbc_safe::*;
