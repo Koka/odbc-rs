fa50ff53a74f67d10e0edcb2d1d94e785f1585c5
zhaokaiyuan <zhaoky0918@gmail.com>
2019-01-21 17:28:27 +0800

    test utf-8 encode bytes in appveyor windows

--- tests/gbk.rs
+++ tests/gbk.rs
@@ -6,7 +6,9 @@ extern crate odbc;
 use odbc::*;
 
 #[test]
-fn exec_direct() {
+#[ignore]
+/// tested in windows中文版
+fn _exec_direct() {
     let env = create_environment_v3().unwrap();
     let conn = env.connect("TestDataSource", "", "").unwrap();
     let stmt = Statement::with_parent(&conn).unwrap();
@@ -25,12 +27,14 @@ fn exec_direct() {
 }
 
 #[test]
-fn prepare_1() {
+#[ignore]
+/// tested in windows中文版
+fn _prepare_1() {
     let env = create_environment_v3().unwrap();
     let conn = env.connect("TestDataSource", "", "").unwrap();
     // select '你好' as hello where 1 = ?
     let stmt = Statement::with_parent(&conn).unwrap().prepare_bytes(vec![115, 101, 108, 101, 99, 116, 32, 39, 196, 227, 186, 195, 39, 32, 97, 115, 32, 104, 101, 108, 108, 111, 32, 119, 104, 101, 114, 101, 32, 32, 49, 32, 61, 32, 63, 32].as_slice()).unwrap();
-    let stmt = stmt.bind_parameter(1, &2).unwrap();
+    let stmt = stmt.bind_parameter(1, &1).unwrap();
 
     match stmt.execute().unwrap() {
         Data(mut stmt) => {
@@ -49,7 +53,9 @@ fn prepare_1() {
 }
 
 #[test]
-fn prepare_2() {
+#[ignore]
+/// tested in windows中文版
+fn _prepare_2() {
     let env = create_environment_v3().unwrap();
     let conn = env.connect("TestDataSource", "", "").unwrap();
     // select '你好' as hello where '你好' = ?
@@ -62,6 +68,71 @@ fn prepare_2() {
         while let Some(mut cursor) = stmt.fetch().unwrap() {
             match cursor.get_data::<Vec<u8>>(1).unwrap() {
                 Some(val) => assert_eq!(val, vec![196, 227, 186, 195]),
+                None => panic!(" NULL"),
+            }
+        }
+    } else {
+        panic!("SELECT did not return result set");
+    }
+}
+
+#[test]
+fn exec_direct() {
+    let env = create_environment_v3().unwrap();
+    let conn = env.connect("TestDataSource", "", "").unwrap();
+    let stmt = Statement::with_parent(&conn).unwrap();
+
+    // select '你好' as hello
+    if let Ok(Data(mut stmt)) = stmt.exec_direct_bytes(vec![115, 101, 108, 101, 99, 116, 32, 39, 228, 189, 160, 229, 165, 189, 39, 32, 97, 115, 32, 104, 101, 108, 108, 111].as_slice()) {
+        while let Some(mut cursor) = stmt.fetch().unwrap() {
+            match cursor.get_data::<Vec<u8>>(1).unwrap() {
+                Some(val) => assert_eq!(val, vec![228u8, 189u8, 160u8, 229u8, 165u8, 189u8]),  // when  你好 is encoded by utf8, it is [228, 189, 160, 229, 165, 189]
+                None => panic!(" NULL"),
+            }
+        }
+    } else {
+        panic!("SELECT did not return result set");
+    }
+}
+
+#[test]
+fn prepare_1() {
+    let env = create_environment_v3().unwrap();
+    let conn = env.connect("TestDataSource", "", "").unwrap();
+    // select '你好' as hello where 1 = ?
+    let stmt = Statement::with_parent(&conn).unwrap().prepare_bytes(vec![115, 101, 108, 101, 99, 116, 32, 39, 228, 189, 160, 229, 165, 189, 39, 32, 97, 115, 32, 104, 101, 108, 108, 111, 32, 119, 104, 101, 114, 101, 32, 49, 32, 61, 32, 63, 32].as_slice()).unwrap();
+    let stmt = stmt.bind_parameter(1, &1).unwrap();
+
+    match stmt.execute().unwrap() {
+        Data(mut stmt) => {
+            while let Some(mut cursor) = stmt.fetch().unwrap() {
+                match cursor.get_data::<Vec<u8>>(1).unwrap() {
+                    Some(val) => assert_eq!(val, vec![228u8, 189u8, 160u8, 229u8, 165u8, 189u8]),
+                    None => panic!(" NULL"),
+                }
+            }
+        }
+        NoData(_) => {
+            panic!("SELECT did not return result set");
+        }
+    }
+
+}
+
+#[test]
+fn prepare_2() {
+    let env = create_environment_v3().unwrap();
+    let conn = env.connect("TestDataSource", "", "").unwrap();
+    // select '你好' as hello where '你好' = ?
+    let stmt = Statement::with_parent(&conn).unwrap().prepare_bytes(vec![115, 101, 108, 101, 99, 116, 32, 39, 228, 189, 160, 229, 165, 189, 39, 32, 97, 115, 32, 104, 101, 108, 108, 111, 32, 119, 104, 101, 114, 101, 32, 39, 228, 189, 160, 229, 165, 189, 39, 32, 61, 32, 63, 32].as_slice()).unwrap();
+    // bind  gbk encoded byte
+    let param = vec![228u8, 189u8, 160u8, 229u8, 165u8, 189u8];
+    let stmt = stmt.bind_parameter(1, &param).unwrap();
+
+    if let Ok(Data(mut stmt)) = stmt.execute() {
+        while let Some(mut cursor) = stmt.fetch().unwrap() {
+            match cursor.get_data::<Vec<u8>>(1).unwrap() {
+                Some(val) => assert_eq!(val, vec![228u8, 189u8, 160u8, 229u8, 165u8, 189u8]),
                 None => panic!(" NULL"),
             }
         }
