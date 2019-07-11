832fe4033dda02f3357723586cb62e4563492915
Konstantin Salikhov <kostya.salikhov@castle.co>
2019-07-11 11:09:36 +0400

    Update docs gh-pages

--- aliases.js
+++ aliases.js
@@ -1,5 +1,6 @@
 var ALIASES = {};
 ALIASES["cfg_if"] = {};
+ALIASES["encoding_rs"] = {};
 ALIASES["log"] = {};
 ALIASES["odbc"] = {};
 ALIASES["odbc_safe"] = {};
--- source-files.js
+++ source-files.js
@@ -1,5 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["cfg_if"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["encoding_rs"] = {"name":"","dirs":[],"files":["ascii.rs","big5.rs","data.rs","euc_jp.rs","euc_kr.rs","gb18030.rs","handles.rs","iso_2022_jp.rs","lib.rs","macros.rs","mem.rs","replacement.rs","shift_jis.rs","single_byte.rs","utf_16.rs","utf_8.rs","variant.rs","x_user_defined.rs"]};
 sourcesIndex["log"] = {"name":"","dirs":[],"files":["lib.rs","macros.rs"]};
 sourcesIndex["odbc"] = {"name":"","dirs":[{"name":"environment","dirs":[],"files":["list_data_sources.rs","mod.rs"]},{"name":"statement","dirs":[],"files":["input.rs","mod.rs","output.rs","prepare.rs","types.rs"]}],"files":["connection.rs","diagnostics.rs","ffi.rs","lib.rs","odbc_object.rs","raii.rs","result.rs"]};
 sourcesIndex["odbc_safe"] = {"name":"","dirs":[{"name":"data_source","dirs":[],"files":["connected.rs","hdbc_wrapper.rs","mod.rs","unconnected.rs"]},{"name":"handles","dirs":[],"files":["hdbc.rs","henv.rs","hstmt.rs","mod.rs"]}],"files":["c_data_type.rs","data_type.rs","diagnostics.rs","environment.rs","indicator.rs","lib.rs","output_buffer.rs","return_.rs","return_option.rs","sql_str.rs","statement.rs","version.rs"]};
