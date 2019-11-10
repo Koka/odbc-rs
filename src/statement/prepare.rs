e9cf5a5ae57d875df947a4e754aef9ff7d1430d4
Konstantin Salikhov <kostya.salikhov@castle.co>
2019-11-11 00:10:05 +0400

    oops

--- src/statement/prepare.rs
+++ src/statement/prepare.rs
@@ -12,7 +12,7 @@ impl<'a, 'b, AC: AutocommitMode> Statement<'a, 'b, Allocated, NoResult, AC> {
     ///
     /// ```rust
     /// # use odbc::*;
-    /// # use odbc_safe::AutocommitOn;
+    /// # use safe::AutocommitOn;
     /// # fn doc() -> Result<()>{
     /// let env = create_environment_v3().map_err(|e| e.unwrap())?;
     /// let conn = env.connect("TestDataSource", "", "")?;
