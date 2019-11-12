ebc4739d33894fbf6389b5a2fc1c9bc36c5b714f
Konstantin Salikhov <kostya.salikhov@castle.co>
2019-11-12 10:27:40 +0400

    Update docs gh-pages

--- implementors/core/ops/drop/trait.Drop.js
+++ implementors/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["odbc_safe"] = [{text:"impl&lt;'env&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"odbc_safe/struct.Connected.html\" title=\"struct odbc_safe::Connected\">Connected</a>&lt;'env&gt;",synthetic:false,types:["odbc_safe::data_source::connected::Connected"]},];
+implementors["odbc_safe"] = [{text:"impl&lt;'env, AC:&nbsp;<a class=\"trait\" href=\"odbc_safe/trait.AutocommitMode.html\" title=\"trait odbc_safe::AutocommitMode\">AutocommitMode</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"odbc_safe/struct.Connected.html\" title=\"struct odbc_safe::Connected\">Connected</a>&lt;'env, AC&gt;",synthetic:false,types:["odbc_safe::data_source::connected::Connected"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
